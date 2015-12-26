68b95240d473c5e58ad0ad707e8247e02ace135f
Bo-Yi Wu <appleboy.tw@gmail.com>
2015-12-26 17:07:40 +0800

    add igmore folder.
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,17 +6,18 @@ git pull origin master;
 
 function doIt() {
   rsync --exclude ".git/" --exclude "bootstrap.sh" \
-    --exclude "README.md" -avh --no-perms . ~; 
+    --exclude "README.md" --exclude "screenshot" \
+    --exclude "freebsd" -avh --no-perms . ~;
   source ~/.bashrc;
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	doIt;
+  doIt;
 else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
-	echo "";
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		doIt;
-	fi;
+  read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
+  echo "";
+  if [[ $REPLY =~ ^[Yy]$ ]]; then
+    doIt;
+  fi;
 fi;
 unset doIt;
