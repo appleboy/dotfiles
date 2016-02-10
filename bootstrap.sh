a925dac8da52dd35328c22de1bbef90f472e731e
Bo-Yi Wu <appleboy.tw@gmail.com>
2016-02-10 17:00:59 +0800

    remove z.sh and git blame command.
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- bootstrap.sh
+++ bootstrap.sh
@@ -21,4 +21,11 @@ else
     doIt;
   fi;
 fi;
+
 unset doIt;
+
+# download z.sh (https://github.com/rupa/z)
+wget https://raw.githubusercontent.com/rupa/z/master/z.sh -O ~/z.sh
+# download git-blame-someone-else (https://github.com/jayphelps/git-blame-someone-else)
+wget https://raw.githubusercontent.com/jayphelps/git-blame-someone-else/master/git-blame-someone-else -O ~/bin/git-blame-someone-else
+chmod 755 ~/bin/git-blame-someone-else
--- update.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-wget https://raw.githubusercontent.com/rupa/z/master/z.sh -O z.sh
-wget https://raw.githubusercontent.com/jayphelps/git-blame-someone-else/master/git-blame-someone-else -O bin/git-blame-someone-else
-
-chmod 755 bin/git-blame-someone-else
