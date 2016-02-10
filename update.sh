1099f9e91f6e9e3cc4023a5bad17398db28097ef
Bo-Yi Wu <appleboy.tw@gmail.com>
2016-02-10 11:28:53 +0800

    add git command and update script.
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

---
+++ update.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+wget https://raw.githubusercontent.com/rupa/z/master/z.sh -O z.sh
+wget https://raw.githubusercontent.com/jayphelps/git-blame-someone-else/master/git-blame-someone-else -O bin/git-blame-someone-else
