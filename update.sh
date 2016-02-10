6e198d8f04656791054f43e0fef3391e47c31b97
Bo-Yi Wu <appleboy.tw@gmail.com>
2016-02-10 12:22:24 +0800

    update script.
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- update.sh
+++ update.sh
@@ -2,3 +2,5 @@
 
 wget https://raw.githubusercontent.com/rupa/z/master/z.sh -O z.sh
 wget https://raw.githubusercontent.com/jayphelps/git-blame-someone-else/master/git-blame-someone-else -O bin/git-blame-someone-else
+
+chmod 755 bin/git-blame-someone-else
