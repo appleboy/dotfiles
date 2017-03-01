b83d5f9dd0dcd5d749e98290bc77951fb5ec0ce6
Bo-Yi Wu <appleboy.tw@gmail.com>
2017-03-01 11:27:35 +0800

    feat: Add wifi-password (#15)
    Get the password of the wifi you're on (bash)
    https://github.com/rauchg/wifi-password

--- bootstrap.sh
+++ bootstrap.sh
@@ -30,46 +30,51 @@ unset doIt;
 # download z.sh (https://github.com/rupa/z)
 [ -f "${HOME}/z.sh" ] && rm -rf ${HOME}/z.sh
 curl -L "https://raw.githubusercontent.com/rupa/z/master/z.sh" -o ~/z.sh
-chmod 755 ~/z.sh
+chmod +x ~/z.sh
 
 # download git-blame-someone-else (https://github.com/jayphelps/git-blame-someone-else)
 curl -o ~/bin/git-blame-someone-else "https://raw.githubusercontent.com/jayphelps/git-blame-someone-else/master/git-blame-someone-else"
-chmod 755 ~/bin/git-blame-someone-else
+chmod +x ~/bin/git-blame-someone-else
 
 # download diff-so-fancy (https://github.com/stevemao/diff-so-fancy)
 [ -d "~/bin/libexec" ] || mkdir -p ~/bin/libexec
 curl -o ~/bin/libexec/diff-so-fancy.pl "https://raw.githubusercontent.com/so-fancy/diff-so-fancy/master/libexec/diff-so-fancy.pl"
 curl -o ~/bin/diff-highlight "https://raw.githubusercontent.com/git/git/master/contrib/diff-highlight/diff-highlight"
 curl -o ~/bin/diff-so-fancy "https://raw.githubusercontent.com/stevemao/diff-so-fancy/master/diff-so-fancy"
-chmod 755 ~/bin/libexec/diff-so-fancy.pl
-chmod 755 ~/bin/diff-highlight
-chmod 755 ~/bin/diff-so-fancy
+chmod +x ~/bin/libexec/diff-so-fancy.pl
+chmod +x ~/bin/diff-highlight
+chmod +x ~/bin/diff-so-fancy
 
 # download git-mv-with-history
 curl -o ~/bin/git-mv-with-history "https://gist.githubusercontent.com/emiller/6769886/raw/git-mv-with-history"
-chmod 755 ~/bin/git-mv-with-history
+chmod +x ~/bin/git-mv-with-history
 
 # See your latest local git branches, formatted real fancy
 # https://github.com/paulirish/git-recent
 curl -o ~/bin/git-recent "https://raw.githubusercontent.com/paulirish/git-recent/master/git-recent"
-chmod 755 ~/bin/git-recent
+chmod +x ~/bin/git-recent
 
 # Type `git open` to open the GitHub page or website for a repository in your browser.
 # https://github.com/paulirish/git-open
 curl -o ~/bin/git-open "https://raw.githubusercontent.com/paulirish/git-open/master/git-open"
-chmod 755 ~/bin/git-open
+chmod +x ~/bin/git-open
 
 # Recall what you did on the last working day. Psst! or be nosy and find what someone else in your team did ;-)
 # https://github.com/kamranahmedse/git-standup
 curl -o ~/bin/git-standup "https://raw.githubusercontent.com/kamranahmedse/git-standup/master/git-standup"
-chmod 755 ~/bin/git-standup
+chmod +x ~/bin/git-standup
 
 # Git quick statistics is a simple and efficient way to access various statistics in git repository.
 # https://github.com/arzzen/git-quick-stats
 curl -o ~/bin/git-quick-stats "https://raw.githubusercontent.com/arzzen/git-quick-stats/master/git-quick-stats"
-chmod 755 ~/bin/git-quick-stats
+chmod +x ~/bin/git-quick-stats
 
 # Simple and handy tool to easily recall what you've done
 # https://github.com/Fakerr/git-recall
 curl -o ~/bin/git-recall "https://raw.githubusercontent.com/Fakerr/git-recall/master/git-recall"
-chmod 755 ~/bin/git-recall
+chmod +x ~/bin/git-recall
+
+# Get the password of the wifi you're on (bash)
+# https://github.com/rauchg/wifi-password
+curl -L https://raw.github.com/rauchg/wifi-password/master/wifi-password.sh -o ~/bin/wifi-password
+chmod +x ~/bin/wifi-password
